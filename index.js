a18bd98312bd406cf4ec3812e4a2cc4c54f56944
bcorporal <bcorporal@gmail.com>
2022-04-05 21:30:14 -0400

    added questions for readme

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer
-  .prompt([
+
+
+const questions = [
     {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+        type: 'input',
+        name: 'title',
+        message: 'Readme Title',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.',
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+        type: 'input',
+        name: 'motivation',
+        message: 'Why did you build this project?',
     },
-  ])
-  .then((response) =>
-  response.confirm === response.password
-    ? console.log('Success!')
-    : console.log('You forgot your password already?!')
-);
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'checkbox',
+        name: 'language',
+        message: 'What language did you include in your app?',
+        choices: ['HTML', 'Javascript', 'Python', 'C', 'Java'],
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+    },
+    {
+        type: 'checkbox',
+        message: 'Chose a license for your product.',
+        name: 'license',
+        choices: ['Creative Commons', 'MIT', 'GNU', 'MySQL'],
+    },
+    {
+        type: 'input',
+        message: 'Enter a link to your github profile.',
+        name: 'github',
+    },
+];
+ 
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                return console.log(err);
+            } else {
+            console.log("Success! Your README.md file has been created.")
+        }
+    });
+}
+    const writeFileAsync = util.promisify(writeToFile);
